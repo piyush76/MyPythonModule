fa1e9b49601bd5eca2d9429e94d8c445783ccef2
piyush <piyush76@gmail.com>
2013-09-16 13:40:31 -0500

    new project structure

--- java-samples/src/com/piyush/learning/basicjava/TestNew.java
+++ java-samples/src/com/piyush/learning/basicjava/TestNew.java
@@ -8,19 +8,27 @@ package com.piyush.learning.basicjava;
  * @author piyush
  */
 public class TestNew {
+
     public static void main(String[] args) {
         TestNew n = null;
         TestNew m = null;
+
+        n = new TestNew();
+        m = new TestNew();
         System.out.println(n);
         System.out.println(m);
-        n = new TestNew();
-        System.out.println(n);
+        n.testMethod();
         m = n;
-        System.out.println(m);
-        n = new TestNew();
         System.out.println(n);
         System.out.println(m);
     }
+
+    public void testMethod() {
+        TestNew global = new TestNew();
+
+        System.out.println("test method " + global);
+    }
+
 }
 
 
