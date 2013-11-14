5c14042e46a58b8e6cc4ac98b74ffd090b01f2c8
piyush <piyush76@gmail.com>
2013-11-13 21:45:26 -0600

    commit

---
+++ java-samples/src/com/piyush/learning/puzzles/TestUniqueCharInString.java
@@ -0,0 +1,71 @@
+package com.piyush.learning.puzzles;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: poonamgupta78
+ * Date: 11/12/13
+ * Time: 11:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Implement an algorithm to determine if a
+ * string has all unique characters.
+ * What if you can not use additional data structures?
+ */
+public class TestUniqueCharInString {
+
+    public static void main(String[] args) {
+
+        String someString = "jackintheboxx" ;
+        System.out.println("Is Unique1 String " +testUnique1(someString))  ;
+        System.out.println("Is Unique2 String " +testUnique2(someString))  ;
+
+
+}
+
+    public static boolean testUnique1(String someString) {
+
+        char[] chars = someString.toCharArray();
+
+        for(int i=0;i<chars.length;i++)  {
+            //System.out.println("I--- "+ chars[i]);
+            for(int j=i+1;j<chars.length;j++){
+                //System.out.println("J---- "+ chars[j]);
+                if(chars[i]==chars[j]){
+                    System.out.println(chars[i] + " equals  " + chars[j]);
+                    return false;
+                }
+            }
+
+        }
+         return true;
+    }
+
+
+    /**
+     * Another way of doing it
+     *
+     * @param someString
+     * @return
+     */
+    public static boolean testUnique2(String someString) {
+
+        boolean[] char_set = new boolean[256];
+
+        for(int i=0;i<someString.length();i++)  {
+            int val = someString.charAt(i) ;
+
+            System.out.println("Val---" + val);
+            if(char_set[val])
+                return false;
+            char_set[val]=true;
+        }
+        return true;
+    }
+
+
+
+
+
+}
