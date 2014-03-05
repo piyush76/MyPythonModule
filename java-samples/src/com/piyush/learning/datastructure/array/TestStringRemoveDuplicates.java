dadff0fbb324095114f8dc2f1329c8f57dd1f302
piyush <piyush76@gmail.com>
2014-03-04 23:12:49 -0600

    commit

--- java-samples/src/com/piyush/learning/datastructure/array/TestStringRemoveDuplicates.java
+++ java-samples/src/com/piyush/learning/datastructure/array/TestStringRemoveDuplicates.java
@@ -6,6 +6,28 @@ package com.piyush.learning.datastructure.array;
  * Date: 11/13/13
  * Time: 11:52 PM
  * To change this template use File | Settings | File Templates.
+ *
+ * Java class to remove duplicates from String
+ *
+ *
  */
 public class TestStringRemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        String someString = "jack in the box";
+        System.out.println("After Removing Duplicates " + removeDuplicates(someString));
+
+
+
+
+    }
+
+    private static String removeDuplicates(String someString) {
+
+
+
+       return null;
+    }
+
 }
