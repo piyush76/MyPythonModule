0ec83c26cb9efca574c538bd39acb055234e1c8b
piyush <piyush76@gmail.com>
2014-05-15 12:10:51 -0500

    committed new code

---
+++ java-samples/src/com/piyush/learning/datastructure/array/LinearSearchArray.java
@@ -0,0 +1,20 @@
+package com.piyush.learning.datastructure.array;
+
+/**
+ * Created by piyush on 5/8/14.
+ */
+public class LinearSearchArray {
+
+/**
+ * Unordered linear search
+ * Sorted/Ordered Linear search
+ *
+ *
+ *
+ *
+*/
+
+
+
+
+}
